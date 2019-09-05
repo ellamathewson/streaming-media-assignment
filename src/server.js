7a9994c581a46a3c88e196c8ac59c777e65a697a
Ella Mathewson (RIT Student) <exm1381@ad.rit.edu>
2019-09-05 17:41:36 -0400

    set up the server, currently not running

--- src/server.js
+++ src/server.js
@@ -3,3 +3,11 @@ const htmlHandler = require('./htmlResponses.js');
 const mediaHandler = require('./mediaResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const onRequest = (request, repsonse) => {
+  console.log(request.url);
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
